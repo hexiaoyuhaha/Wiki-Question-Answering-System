1ab6a40e0e128ca57450440e2aa645852ffa15e4
Shuang0420 <sxu1@andrew.cmu.edu>
2017-03-10 16:47:43 -0500

    stanford parser

--- Helper.py
+++ Helper.py
@@ -2,20 +2,21 @@ from __future__ import division, unicode_literals
 import nltk
 from textblob import TextBlob as tb
 import math
+from nltk.tag.stanford import StanfordPOSTagger
+from nltk.tag import StanfordNERTagger
+from nltk.parse.stanford import StanfordParser
+from nltk.parse.stanford import StanfordDependencyParser
 
 corpus = []
 doc1 = "I love Chinese food"
 doc2 = "I love American spirits"
 corpus.append(doc1)
 corpus.append(doc2)
-print 'tf',tf('love',doc1)
-print 'idf',idf('love', corpus)
-print 'tfidf',tfidf('love',doc1,corpus)
+
 
 def getTf(word, blob):
     blob = tb(blob.strip())
     return blob.words.count(word) / len(blob.words)
-
 
 
 def n_containing(word, bloblist):
@@ -25,5 +26,31 @@ def getIdf(word, bloblist):
     bloblist = [tb(doc.strip('\n')) for doc in corpus]
     return math.log(len(bloblist) / n_containing(word, bloblist)) + 1
 
-def getTfIdf(word, blob, bloblist):
-    return tf(word, blob) * idf(word, bloblist)
+def getTfidf(word, blob, bloblist):
+    return getTf(word, blob) * getIdf(word, bloblist)
+
+def getPos(line):
+    st = StanfordPOSTagger('english-bidirectional-distsim.tagger')
+    print st.tag('What is the airspeed of an unladen swallow ?'.split())
+
+def getNER(line):
+    st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
+    print st.tag('Rami Eid is studying at Stony Brook University in NY'.split())
+
+
+def getParserTree(line):
+    parser=StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+    print list(parser.raw_parse("the quick brown fox jumps over the lazy dog"))
+
+def getDependencyTree(line):
+    dep_parser=StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+    print [parse.tree() for parse in dep_parser.raw_parse("The quick brown fox jumps over the lazy dog.")]
+
+print 'tf',getTf('love',doc1)
+print 'idf',getIdf('love', corpus)
+print 'tfidf',getTfidf('love',doc1,corpus)
+
+#getPos(doc1)
+#getNER(doc1)
+getParserTree(doc1)
+#getDependencyTree(doc1)

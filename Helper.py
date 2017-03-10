18872c0a8b87c2746122098c764d905790925fb3
Shuang0420 <sxu1@andrew.cmu.edu>
2017-03-10 16:30:23 -0500

    tfidf

--- Helper.py
+++ Helper.py
@@ -12,7 +12,7 @@ print 'tf',tf('love',doc1)
 print 'idf',idf('love', corpus)
 print 'tfidf',tfidf('love',doc1,corpus)
 
-def tf(word, blob):
+def getTf(word, blob):
     blob = tb(blob.strip())
     return blob.words.count(word) / len(blob.words)
 
@@ -21,9 +21,9 @@ def tf(word, blob):
 def n_containing(word, bloblist):
     return sum(1 for blob in bloblist if word in blob.words)
 
-def idf(word, bloblist):
+def getIdf(word, bloblist):
     bloblist = [tb(doc.strip('\n')) for doc in corpus]
     return math.log(len(bloblist) / n_containing(word, bloblist)) + 1
 
-def tfidf(word, blob, bloblist):
+def getTfIdf(word, blob, bloblist):
     return tf(word, blob) * idf(word, bloblist)
